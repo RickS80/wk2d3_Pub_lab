9d071884c373aaf6a5dcb71d87ecaef5c38ce340
Rick Symington <rick.symington@gmail.com>
2018-11-28 10:50:40 +0000

    Add initial files

---
+++ specs/Customer_spec.rb
@@ -0,0 +1,8 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../Customer')
+
+class CustomerTest < Minitest::Test
+
+
+end 
---
+++ specs/Drink_spec.rb
@@ -0,0 +1,8 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../Drink')
+
+class DrinkTest < Minitest::Test
+
+
+end
